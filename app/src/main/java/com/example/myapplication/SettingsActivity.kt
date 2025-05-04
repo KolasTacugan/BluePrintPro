94ae963114548bb62769b5bfef448af9e34a9e8e
Gene Harold <geneisme20@gmail.com>
2025-05-04 12:18:29 +0800

    LOGIN(80%)

--- app/src/main/java/com/example/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -12,22 +12,6 @@ class SettingsActivity : Activity() {
 
         setContentView(R.layout.activity_settings)
 
-        val button_edit_profile: Button = findViewById(R.id.button_edit_profile)
-        val button_pagehome: Button = findViewById(R.id.button_pagehome)
-        val button_logout: Button = findViewById(R.id.button_logout)
 
-        button_edit_profile.setOnClickListener{
-            val intent = Intent(this, EditProfileActivity::class.java)
-            startActivity(intent)
-        }
-
-        button_pagehome.setOnClickListener{
-            val intent = Intent(this, LandingActivity::class.java)
-            startActivity(intent)
-        }
-        button_logout.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
