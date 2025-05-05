45d3511e78d347ce4f723c127fc918b102baed48
Gene Harold <geneisme20@gmail.com>
2025-05-05 23:22:37 +0800

    LOGOUT KUNOHAY

--- app/src/main/java/com/example/myapplication/LandingActivity.kt
+++ app/src/main/java/com/example/myapplication/LandingActivity.kt
@@ -1,32 +1,22 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 
 class LandingActivity : Activity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_landing2)
 
-        val button_profile: Button = findViewById(R.id.button_profile)
-        val button_developers_page: Button = findViewById(R.id.button_developers_page)
-        val button_settings: Button = findViewById(R.id.button_settings)
+        val logOut = findViewById<Button>(R.id.btn_viewProfile)
 
-        button_profile.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
-
-        button_developers_page.setOnClickListener {
-            val intent = Intent(this, DevelopersActivity::class.java)
-            startActivity(intent)
-        }
-
-        button_settings.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
+        logOut.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
