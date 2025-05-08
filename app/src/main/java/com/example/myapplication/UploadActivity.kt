ad0202cd034906af11cabb1df28e54466e13ae3e
KolasTacugan <nicoletacuganbsit@gmail.com>
2025-05-08 22:01:12 +0800

    Marketpplace

---
+++ app/src/main/java/com/example/myapplication/UploadActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UploadActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_upload)
+
+    }
+}
