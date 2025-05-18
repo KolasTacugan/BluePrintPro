20467619c027627e1e7039308c4f36b9517671ff
Samantha <samrubica@gmail.com>
2025-05-18 10:52:54 +0800

    Sam Changes

---
+++ app/src/main/java/com/example/myapplication/ProjectActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat.Type
+
+class ProjectActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project)
+
+    }
+}
+
