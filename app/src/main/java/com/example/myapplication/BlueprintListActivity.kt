67c7fe318f65454275a971a6e7ec17762acdb254
Samantha <samrubica@gmail.com>
2025-05-19 22:14:32 +0800

    Sam Changes

---
+++ app/src/main/java/com/example/myapplication/BlueprintListActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.widget.FrameLayout
+
+class BlueprintListActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blueprint_list)
+
+    }
+}
